3add3fa58a87a853875a0783baa967f5a156ccf0
serg1557733 <serg1557733@ukr.net>
2021-07-26 15:04:03 +0300

    home work

--- script.js
+++ script.js
@@ -22,8 +22,28 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
+/* let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
 
-let num = '20';
+
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies : {
+    },
+    actors : {},
+    genres :[],
+    privat : false
+};
+
+let film1 = prompt('Один из последних просмотренных фильмов?','');
+let rating1 = prompt('На сколько оцените его?','');
+let film2 = prompt( 'Один из последних просмотренных фильмов?','');
+let rating2 = prompt('На сколько оцените его?','');
+personalMovieDB.movies[film1] =  rating1;
+personalMovieDB.movies[film2] =  rating2;
+console.log(numberOfFilms);
+console.log(personalMovieDB); */
+/* 
+let num = 50;
 
 if (num < 49) {
     console.log('error');
@@ -45,7 +65,95 @@ switch (num) {
     case 40:
         console.log('yes');
         break;
-    default:  console.log('default');
-    
-        
+    default:  console.log('default');  
+} */
+
+/* while (num <= 55) {
+    console.log(num);
+    num++;
+} */
+
+/* do {
+    console.log(num);
+    num++;
 }
+while (num < 55)  */
+
+/* for (let i = 1; i<10; i++) {
+    if (i === 2n){
+        //break;
+        continue;
+    }
+    console.log(i);
+} */
+/* Задание на урок:
+
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies : {
+    },
+    actors : {},
+    genres :[],
+    privat : false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?','');
+    let b = prompt('На сколько оцените его?','');
+   if (a !=='' && b !=='' && a !== null && b!== null && a.length < 50 && b.length < 50) {
+    personalMovieDB.movies[a] =  b;
+    console.log('Done');
+   } 
+   else {
+    console.log('error');
+    i--;
+   }
+}
+
+console.log(personalMovieDB);
+/* 
+if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+}
+else if ( personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+}
+else   {
+    console.log("Произошла ошибка");
+} */
+
+while (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+    break;
+}
+while (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+    break;
+}
+while (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+    break;
+}
+
+
+console.log(personalMovieDB.count);
